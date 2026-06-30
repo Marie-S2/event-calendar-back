de11a956cfde020c336793f9c32c699dc863369f
Marie-S2 <smariesabdousophie@gmail.com>
2026-06-30 16:01:18 +0000

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -2,26 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'https://event-calendar-front.vercel.app'
+    ],
 
     'allowed_headers' => ['*'],
 
@@ -29,7 +17,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
-
