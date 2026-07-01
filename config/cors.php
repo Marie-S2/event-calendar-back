82156e587c9e5a51ec0db86f09515577716cc365
Marie-S2 <smariesabdousophie@gmail.com>
2026-07-01 08:31:22 +0000

    fix CORS origins

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://event-calendar-front-d8ybfm7cu-poh123.vercel.app/login'
+       'http://localhost:4200',
+        'https://event-calendar-front.vercel.app',
+        'https://event-calendar-front-d8ybfm7cu-poh123.vercel.app',
     ],
 
     'allowed_headers' => ['*'],
